bbd115a4abe657688b46b17065a4a02d252aa5d2
Finn Julius Stephansen-Smith <finni-94@hotmail.com>
2019-02-13 18:56:36 +0900

    [Experimental] Increase the size of the rendered SVGs

--- src/render/render_stream_graph.py
+++ src/render/render_stream_graph.py
@@ -13,53 +13,53 @@ def render_stream_graph(ordered_nodes, timestamps, svg_path='stream_graph.svg'):
 
     include_unordered_example = True
 
-    initial_y_offset = 11
-    height = initial_y_offset + 4 + len(ordered_nodes) * 4
-    size = (135, height) if include_unordered_example else (65, 60)
+    initial_y_offset = 110
+    height = initial_y_offset + 40 + len(ordered_nodes) * 40
+    size = (1350, height) if include_unordered_example else (650, height)
     g = svg.Drawing(svg_path, size=size, profile='full')
-    g.add(g.text('Stream graph ordering', insert=(3, 5),
-                 fill='black', font_size='4', font_family='Gill Sans, sans-serif', font_weight=100))
+    g.add(g.text('Stream graph ordering', insert=(30, 50),
+                 fill='black', font_size='40', font_family='Gill Sans, sans-serif', font_weight=100))
 
     # ########################## Nodes ################################
 
     def render_nodes(nodes_to_render, x_offset):
         for i, node in enumerate(nodes_to_render):
-            y_offset = (i * 4) + initial_y_offset
+            y_offset = (i * 40) + initial_y_offset
             add_background_lines(g, x_offset, y_offset)
-            g.add(g.text(node, insert=(x_offset, y_offset + 1), fill='black', font_size='2.25', font_family='Verdana'))
+            g.add(g.text(node, insert=(x_offset, y_offset + 10), fill='black', font_size='22.5', font_family='Verdana'))
 
     if include_unordered_example:
-        render_nodes(sorted(ordered_nodes), 3)
+        render_nodes(sorted(ordered_nodes), 30)
 
         # Add arrow image between ordered and unordered
         arrow_resize_factor = 200
         path = '' if __name__ == '__main__' else '../render/'
-        g.add(svg.image.Image(href=path + 'arrow-drawing-1.png', insert=(63, height / 2),
+        g.add(svg.image.Image(href=path + 'arrow-drawing-1.png', insert=(630, height / 2),
                               size=(1200 / arrow_resize_factor, 1059 / arrow_resize_factor)))
 
-    render_nodes(ordered_nodes, 75 if include_unordered_example else 3)
+    render_nodes(ordered_nodes, 750 if include_unordered_example else 30)
 
     # ########################## Edges ################################
 
     def render_edges(timestamps_to_render, x_offset, ordered=False):
         for time, edge_set in enumerate(timestamps_to_render):
             for edge in edge_set:
-                x = x_offset + time * 10
+                x = x_offset + time * 100
                 y = get_y_offset_of_node(edge[0], ordered)
                 target_y = get_y_offset_of_node(edge[1], ordered)
                 middle = (y + target_y) / 2
                 curve_radius_offset = get_curve_radius_offset(y, target_y)
 
-                p = g.path(d=('m', x, y), stroke='black', fill='none', stroke_width=0.25)
+                p = g.path(d=('m', x, y), stroke='black', fill='none', stroke_width=02.5)
                 p.push('Q', x + curve_radius_offset, middle, x, target_y)
                 g.add(p)
 
-                g.add(svg.shapes.Circle(center=(x, y), r=0.4, stroke_width=0.1, fill='black', stroke='black'))
-                g.add(svg.shapes.Circle(center=(x, target_y), r=0.4, stroke_width=0.1, fill='black', stroke='black'))
+                g.add(svg.shapes.Circle(center=(x, y), r=4, stroke_width=1, fill='black', stroke='black'))
+                g.add(svg.shapes.Circle(center=(x, target_y), r=4, stroke_width=1, fill='black', stroke='black'))
 
     def get_y_offset_of_node(node, ordered):
         i = sorted(ordered_nodes).index(node) if ordered else ordered_nodes.index(node)
-        return initial_y_offset + i * 4
+        return initial_y_offset + i * 40
 
     # get_curve_radius_offset is a function that takes the y-coordinate of two nodes, and outputs information about
     # how the curve between them should look if they are connected. Specifically, it returns the radius of the curve.
@@ -68,20 +68,20 @@ def render_stream_graph(ordered_nodes, timestamps, svg_path='stream_graph.svg'):
         # 60 is about the distance of 15 nodes, and a "t" (function time variable) that maxes around 10
         # provides a suitable curve with our chosen function. Thus, we divide the difference between the y-coords,
         # our only input indicator for how the curve radius should grow, by the "max" distance divided by 10.
-        t = abs(target_y - y) / 6
+        t = abs(target_y - y) / 20
 
         # We want an asymptotic function for the curve radius, since it should never cross into the area of the next
         # timestamp. The expansion of 2 and constant of 5 were found experimentally, and are used to control how
         # quickly the radius should grow with distance.
-        normal_asymptotic = (t ** 2) / ((t ** 2) + 5)
+        normal_asymptotic = (t ** 2) / ((t ** 2) + 50)
 
         # This 15 is derived from the 10 used as x-seperator for each time step. For some reason a radius of 10
         # covers "less distance" that the distance of 10 to the next time step. 15 provides a nice boundary,
         # preserving a small padding to the next time step even if normal_asymptotic approaches its max value, 1.
-        return normal_asymptotic * 15
+        return normal_asymptotic * 150
 
-    render_edges(timestamps, 12, True)
-    render_edges(timestamps, 87)
+    render_edges(timestamps, 120, True)
+    render_edges(timestamps, 870)
 
     g.save()
     if __name__ == '__main__':
@@ -90,11 +90,11 @@ def render_stream_graph(ordered_nodes, timestamps, svg_path='stream_graph.svg'):
 
 def add_background_lines(g, x, y):
     # Configuration for the background lines
-    bg_stroke_width = 0.15
+    bg_stroke_width = 01.5
     bg_stoke = 'black'
-    bg_length = 1
-    bg_space = 1
-    bg_x_offset = x + 5
+    bg_length = 10
+    bg_space = 10
+    bg_x_offset = x + 50
 
     for i in range(25):
         x_offset = bg_x_offset + (bg_length + bg_space) * i
@@ -104,7 +104,7 @@ def add_background_lines(g, x, y):
 def viewer(size, svg_path):
     app = QApplication(sys.argv)
     svg_widget = QtSvg.QSvgWidget(svg_path)
-    svg_widget.setGeometry(size[0], size[1], size[0] * 10, size[1] * 10)
+    svg_widget.setGeometry(size[0], size[1], size[0] * 100, size[1] * 100)
     # svg_widget.setGeometry(50, 50, 759, 668)
     svg_widget.show()
 
