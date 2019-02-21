d62359f79ed2b3de4f5fb30fac83f5da7104c130
Finn Julius Stephansen-Smith <finni-94@hotmail.com>
2019-02-21 14:56:42 +0900

    Basic two-pay partitioning, initial solution

--- src/partition/half.py
+++ src/partition/half.py
@@ -14,6 +14,7 @@ if __name__ == '__main__' and __package__ is None:
 
 from generator import sgdataset
 from generator import SG20190122
+from render.render_stream_graph import render_stream_graph
 
 logging.basicConfig(
     level=logging.INFO,
@@ -35,35 +36,57 @@ def split(n_vertices, timesteps):
     for node in partition:
         optimizer.add(Or(node == 0, node == 1))
 
-    for x in partition:
-        print('x', x, (x == 0), x == 1)
-    print('asd', [x for x in range(n_vertices) if partition[x] == 0])
-    partition_0 = [partition[x] for x in range(n_vertices) if partition[x] == 0]
-    partition_1 = [partition[x] for x in range(n_vertices) if partition[x] == 1]
-    size_difference_threshold = int(n_vertices / 10)
-    optimizer.add(Abs(Sum(partition) - int(n_vertices / 2)) <= size_difference_threshold)
+    partition_0_len = Int('0_len')
+    partition_1_len = Int('1_len')
+
+    optimizer.add(partition_1_len == Sum([If(partition[x] == 1, 1, 0) for x in range(n_vertices)]))
+    optimizer.add(partition_0_len == n_vertices - partition_1_len)
 
     # Keep the size of the partitions within a certain threshold of each other
-    optimizer.add(Abs(len(partition_0) - len(partition_1)) <= size_difference_threshold)
+    size_difference_threshold = int(n_vertices / 5)
+    optimizer.add(Abs(partition_0_len - partition_1_len) <= size_difference_threshold)
 
     edges = sum(timesteps, [])
-    edges_between_partitions = [If(And(partition[x] != partition[y], (x, y) in edges), 1, 0)
-                                for x in range(n_vertices) for y in range(n_vertices) if y != x]
+    print('edges', edges)
+    edges_between_partitions = [Int('e{}_{}'.format(i, j)) for i in range(n_vertices) for j in range(n_vertices)]
+
+    def get_edge(u, v):
+        for single_edge in edges_between_partitions:
+            if str(single_edge) == 'e{}_{}'.format(u, v):
+                return single_edge
+        raise RuntimeError('Tried finding edge between node ' + str(u) + ' and node ' + str(v)
+                           + ', but it did not exist.')
+
+    for edge_pair in edges_between_partitions:
+        optimizer.add(Or(edge_pair == 0, edge_pair == 1))
+
+    optimizer.add([Implies(And(partition[x] != partition[y], Or([x, y] in edges, [y, x] in edges)), get_edge(x, y) ==  1)
+                  for x in range(n_vertices) for y in range(n_vertices)])
     optimizer.minimize(Sum(edges_between_partitions))
 
-    return optimizer, partition, partition_0, partition_1
+    return optimizer, partition
 
 
 if __name__ == '__main__':
     t = time.time()
-    dataset = sgdataset.AbstractDataset.load('ErdosRenyi')
-    logging.debug(dataset['kind'], dataset['doc'])
+    print('Starting partitioning...')
+    dataset = sgdataset.AbstractDataset.load('sample')
     for data in dataset['dataset']:
         params = data['params']
-        optimizer, partition, p0, p1 = split(params['n_vertices'], data['content'])
-        optimizer.check()
-        print('Finished', p0, p1)
-        m = optimizer.model()
-        r = [m.evaluate(partition[i]) for i in range(len(partition))]
-        d = [(i, x.as_long()) for i, x in enumerate(r)]
-        print('model', m, r)
+        optimizer, partition = split(params['n_vertices'], data['content'])
+        result = optimizer.check()
+        if result == sat:
+            print('Finished!')
+            m = optimizer.model()
+            r = [m.evaluate(partition[i]) for i in range(len(partition))]
+            d = [(i, x.as_long()) for i, x in enumerate(r)]
+
+            sorted_nodes = list(map(lambda y: y[0], sorted(d, key=lambda x: x[1])))
+            print('Result:', m)
+
+            edges = sum(data['content'], [])
+            edges = [tuple(e) for e in edges]
+            print('Rendering', sorted_nodes, data['content'])
+            render_stream_graph(sorted_nodes, data['content'])
+        else:
+            print('Could not solve')
